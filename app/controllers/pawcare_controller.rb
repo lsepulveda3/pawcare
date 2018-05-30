b985b354cfa61ae364c158f79075b2581d9b490d
Lizeth Sepulveda <lsepulveda3@cps.edu>
2018-05-30 14:03:11 +0000

    Add links to todos on the index page

--- app/controllers/pawcare_controller.rb
+++ app/controllers/pawcare_controller.rb
@@ -1,4 +1,18 @@
 class PawcareController < ApplicationController
     def index
     end
+    
+    def show
+        
+       pawcare_id = '2'
+  if pawcare_id == '1'
+    @pawcare_description = "Make the curriculum"
+    @pawcare_cost = 4
+  elsif pawcare_id == '2'
+    @pawcare_description = "Buy workshop supplies"
+    @pawcare_cost = 3
+  end
+
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
 get 'pawcare/index', to: 'pawcare#index'
+get 'pawcare/show', to: 'pawcare#show'
 end
